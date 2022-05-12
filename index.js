18540f564c86ce4a0309fbd9372dd4ff5051821d
John Morgan II <102772504+JohnMrgn93@users.noreply.github.com>
2022-05-11 21:45:44 -0500

    upload

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 import { Header, Nav, Main, Footer } from "./components";
+import * as Store from "./Store";
+import Navigo from "navigo";
+import { capitalize } from "lodash";
 
-function render() {
+const router = new Navigo("/");
+
+function render(state = Store.Home) {
   document.querySelector("#root").innerHTML = `
-    ${Header()}
-    ${Nav()}
-    ${Main()}
+    ${Header(state)}
+    ${Nav(Store.Links)}
+    ${Main(state)}
     ${Footer()}
   `;
-
-  afterRender();
+  router.updatePageLinks();
+  AfterRender();
 }
 
-render();
-
-function afterRender() {
+function AfterRender() {
   // add menu toggle to bars icon in nav bar
   document
     .querySelector(".fa-bars")
@@ -21,3 +24,13 @@ function afterRender() {
       document.querySelector("nav > ul").classList.toggle("hidden--mobile")
     );
 }
+
+router
+  .on({
+    "/": () => render(),
+    ":view": params => {
+      let view = capitalize(params.data.view);
+      render(Store[view]);
+    }
+  })
+  .resolve();
