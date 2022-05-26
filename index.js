d49dad67d7debfa0f251d3259abec32c352f4521
John Morgan II <102772504+JohnMrgn93@users.noreply.github.com>
2022-05-25 21:19:30 -0500

    Upload class spa

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import { Header, Nav, Main, Footer } from "./components";
 import * as Store from "./Store";
 import Navigo from "navigo";
 import { capitalize } from "lodash";
-
+import axios from "axios";
+import dotenv from "dotenv";
 const router = new Navigo("/");
 
 function render(state = Store.Home) {
@@ -24,6 +25,52 @@ function AfterRender() {
       document.querySelector("nav > ul").classList.toggle("hidden--mobile")
     );
 }
+
+router.hooks({
+  before: (done, params) => {
+    let view = "Home";
+    if (params && params.data && params.data.view) {
+      view = capitalize(params.data.view);
+    }
+
+    if (view === "Home") {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st.%20louis`
+        )
+        .then(response => {
+          const kelvinToFahrenheit = kelvinTemp =>
+            Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
+
+          Store.Home.weather = {};
+          Store.Home.weather.city = response.data.name;
+          Store.Home.weather.temp = kelvinToFahrenheit(response.data.main.temp);
+          Store.Home.weather.feelsLike = kelvinToFahrenheit(
+            response.data.main.feels_like
+          );
+          Store.Home.weather.description = response.data.weather[0].main;
+          done();
+        })
+        .catch(err => {
+          console.log(err);
+          done();
+        });
+    } else if (view === "Pizza") {
+      axios
+        .get(`${process.env.PIZZA_PLACE_API_URL}`)
+        .then(response => {
+          Store.Pizza.pizzas = response.data;
+          done();
+        })
+        .catch(error => {
+          console.log("It puked", error);
+          done();
+        });
+    } else {
+      done();
+    }
+  }
+});
 
 router
   .on({
